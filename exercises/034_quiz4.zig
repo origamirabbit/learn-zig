4d4f5a5710f1d8388b24a72e3a92bb533b13ae19
riverliu <30144128+origamirabbit@users.noreply.github.com>
2024-02-22 00:24:59 +0800

    from 001 to 043

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,13 +9,23 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+// Using try
+pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num: u32 = try getNumber();
 
     try stdout.print("my_num={}\n", .{my_num});
 }
+
+// Alternatively, using catch
+// pub fn main() void {
+//     const stdout = std.io.getStdOut().writer();
+
+//     const my_num: u32 = getNumber() catch 42;
+
+//     stdout.print("my_num={}\n", .{my_num}) catch {};
+// }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
 fn getNumber() NumError!u32 {
